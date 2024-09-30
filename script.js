4b530f68a14c146be18d613b6bf09e7a1f0eb8cb
Mowdat <zrida4534@gmail.com>
2024-09-30 13:19:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+left.addEventListener('mouseover', () => container.classList.add('hover-left'))
+left.addEventListener('mouseout', () => container.classList.remove('hover-left'))
 
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+right.addEventListener('mouseover', () => container.classList.add('hover-right'))
+right.addEventListener('mouseout', () => container.classList.remove('hover-right'))
+
